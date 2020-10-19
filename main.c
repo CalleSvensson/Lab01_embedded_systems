0ca07648b633cbd8d821e9224cdbe53cab12b087
calle <lordcarlviktor@hotmail.com>
2020-10-19 13:47:03 +0200

    Implemented functions uart_init(), uart_putchar() and uart_putstr. Moved the ed blink function to a separte function and added blue and green led

--- main.c
+++ main.c
@@ -6,14 +6,40 @@
 #include <stdlib.h>
 #include "serial.h"
 
+#define REDLED PB3;
+ 
+void ledblink()
+{
+    DDRB |= (1 << PB3); // set pin 11 on Arduino board to output
+    DDRB |= (1 << PB2); // set pin 10 on Arduino board to output
+    DDRB |= (1 << PB1); // set pin 9 on Arduino board to output
+
+    while (true)
+    {
+        PORTB |= (1 << PB3);// set pin 11 on Arduino board to HIGH
+        _delay_ms(500);
+        PORTB &= ~(1 << PB3);// set pin 11 on Arduino board to LOW
+        _delay_ms(500);        
+        PORTB |= (1 << PB2);// set pin 10 on Arduino board to HIGH
+        _delay_ms(500);
+        PORTB &= ~(1 << PB2);// set pin 10 on Arduino board to LOW
+        _delay_ms(500);        
+        PORTB |= (1 << PB1);// set pin 9 on Arduino board to HIGH
+        _delay_ms(500);
+        PORTB &= ~(1 << PB1);// set pin 9 on Arduino board to LOW
+        _delay_ms(500);
+    }
+}
 void main(void) {
-	DDRB |= (1 << PB3); // set pin 11 on Arduino board to output
-
-	while (true) {
-		PORTB |= (1 << PB3);// set pin 11 on Arduino board to HIGH
-		_delay_ms(500);
-		PORTB &= ~(1 << PB3);// set pin 11 on Arduino board to LOW
-		_delay_ms(500);
-	}
-
+    //ledblink();
+    uart_init();
+    while (1)
+    {
+        /*uart_putchar('c');
+        _delay_ms(500);
+        uart_putchar('\n');
+        _delay_ms(500);*/
+        uart_putstr("hej calle \n");
+        _delay_ms(500);
+    }
 }
--- serial.c
+++ serial.c
@@ -2,3 +2,51 @@
 #include <util/delay.h>
 
 #include "serial.h"
+
+
+#define FOSC 16000000  // Clock speed
+#define BAUD 38400    // Baud rate
+#define MYUBRR  ((FOSC / 16) / BAUD) -1  //  Calculating baud rate register setting 
+
+void uart_init(void) 
+{
+    UBRR0L = (uint8_t)(MYUBRR & 0xFF); // write to lower byte
+    UBRR0H = (uint8_t)(MYUBRR >> 8);// write to higher byte
+    UCSR0C = 0x06;  // Set to 8N1
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0); // enable the transmitter(TX) and receiver(RX)
+}
+
+void uart_putchar(char chr)
+{
+    if (chr == '\n') // Check if the char is a new line
+    {
+        while (!(UCSR0A & (1 << UDRE0))); // Wait for empty transmit buffer
+        UDR0 = '\r';
+        while (!(UCSR0A & (1 << UDRE0)));
+        UDR0 = '\n';
+    }
+    else
+    {
+        while (!(UCSR0A & (1 << UDRE0)));
+        UDR0 = chr;  // Send char
+    }
+}
+
+void uart_putstr(const char* str)
+{
+    int i = 0;
+    while (str[i] != 0)  // Check for the end of the String
+    {
+        uart_putchar(str[i]);  // Send each char 
+        i++;
+    }    
+}
+
+char uart_getchar(void)
+{
+	return 0;
+}
+
+void uart_echo(void)
+{
+}
